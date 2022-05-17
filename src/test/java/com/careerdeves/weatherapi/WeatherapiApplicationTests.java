655ebd6cc0d664fa600611e0da60083e2c882bc1
wmaf <wmaf@outlook.com>
2022-05-17 18:21:24 -0400

    Initial commit

---
+++ src/test/java/com/careerdeves/weatherapi/WeatherapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdeves.weatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
