501737ed267a7d6bfec49289bdea8f63a62a78c1
wmaf <wmaf@outlook.com>
2022-05-23 12:36:08 -0400

    ide error

---
+++ src/main/java/com/careerdeves/weatherapi/controllers/CurrentWeatherController.java
@@ -0,0 +1,49 @@
+package com.careerdeves.weatherapi.controllers;
+
+
+import com.careerdeves.weatherapi.models.CurrentWaether;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/current")
+public class CurrentWeatherController {
+    @Autowired
+    private Environment environment;
+// create a base url  final, so it can't be changed  note the format
+    private final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+
+
+
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<?> getCurrentWeatherByCity (RestTemplate restTemplate, @PathVariable String cityName) {
+        try {
+            String apiKey = environment.getProperty("OW_API_KEY");
+            //String city = "providence";
+            String querryString = "?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+            String openWeatherUrl = BASE_URL + querryString;
+
+           CurrentWaether openWeatherResponse = restTemplate.getForObject(openWeatherUrl, CurrentWaether.class);
+
+
+            return ResponseEntity.ok(openWeatherResponse);
+
+
+        }   catch (HttpClientErrorException.NotFound e) {
+            return ResponseEntity.status(404).body("City Not Found!  You entered: " + cityName);
+        }   catch ( Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
+    }
+}
