e44619ededa5d015f093f1b5c7b3b5fd9a3f21e3
wmaf <wmaf@outlook.com>
2022-06-13 15:52:39 -0400

    Weather API update.

--- src/main/java/com/careerdeves/weatherapi/controllers/ForecastController.java
+++ src/main/java/com/careerdeves/weatherapi/controllers/ForecastController.java
@@ -1,5 +1,6 @@
 package com.careerdeves.weatherapi.controllers;
 
+import com.careerdeves.weatherapi.models.ForcastReport;
 import com.careerdeves.weatherapi.models.Forecast;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -18,24 +20,32 @@ public class ForecastController {
     private Environment env;
     private final String BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
-    @GetMapping("/city/{city}")
-    public ResponseEntity<?> getForecastByCity (RestTemplate restTemplate, @PathVariable String city) {
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<?> getForecastByCity (RestTemplate restTemplate, @PathVariable String cityName) {
 
         try {
             String units = "imperial";
             String  apiKey = env.getProperty("OW_API_KEY");
-            String queryString = "?q=" + city + "$units=" + units + "&appid=" + apiKey;
-            String url = BASE_URL + queryString;
+            String queryString = "?q=" + cityName + "&units=" + units + "&appid=" + apiKey;
+            String forecastURL = BASE_URL + queryString;
+            //System.out.println(forecastURL);
+            Forecast owRes = restTemplate.getForObject(forecastURL, Forecast.class);
 
-            Forecast owRes = restTemplate.getForObject(url, Forecast.class);
+            //Generate report removing null entries.
 
-            //Generate report
+            assert owRes != null;
+            ForcastReport report = new ForcastReport(owRes);
+
+
             return ResponseEntity.ok(owRes);
 
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-            System.out.println(e.getClass());
-            return ResponseEntity.internalServerError().body(e.getMessage());
+
+        }   catch (HttpClientErrorException.NotFound e) {
+                return ResponseEntity.status(404).body("City Not Found!  You entered: " + cityName);
+        }   catch ( Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println(e.getClass());
+                return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
---
+++ src/main/java/com/careerdeves/weatherapi/models/ForcastReport.java
@@ -0,0 +1,70 @@
+package com.careerdeves.weatherapi.models;
+
+import com.careerdeves.weatherapi.models.Forecast.ForecastWeather;
+
+public class ForcastReport {
+
+    private final String cityName;
+    private final String country;
+    private final int population;
+    private ForecastReportEntry[] reports;
+
+    public ForcastReport(Forecast forecast) {
+        cityName = forecast.getCity().getName();
+        country = forecast.getCity().getCountry();
+        population = forecast.getCity().getPopulation();
+
+        reports = new   ForecastReportEntry[forecast.getList().length];
+
+        for (int i = 0; i < forecast.getList().length; i++) {
+            reports[i] = new ForecastReportEntry(forecast.getList()[i]);
+
+        }
+    }
+
+    public static class ForecastReportEntry{
+        private final String dateTime;
+        private final String description;
+        private final String temp;
+        private final String percentageOfPrecipitation;
+
+    public ForecastReportEntry(Forecast.ForecastWeather wd) {
+        description = wd.getWeather()[0].getMain() + " : " + wd.getWeather()[0].getDescription();
+        dateTime = wd.getDt_txt();
+        temp = wd.getMain().getTemp() + "°F";  // needs to be refactored to reflect proper data type
+        percentageOfPrecipitation = (wd.getPop() * 100) + "%";
+    }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getDateTime() {
+            return dateTime;
+        }
+
+        public String getTemp() {
+            return temp;
+        }
+
+        public String getPercentageOfPrecipitation() {
+            return percentageOfPrecipitation;
+        }
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public ForecastReportEntry[] getReports() {
+        return reports;
+    }
+}
